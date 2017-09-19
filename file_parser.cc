5201352436da303945c53eab75dd1863337cbe14
Jon Verne <jonverne@users.noreply.github.com>
2017-09-19 16:16:51 -0700

    Update file_parser.cc
    Added file_name variable to hold argv[1], added a file_parser and a line_parser method, renamed the vector in the print_file to victor.

--- file_parser.cc
+++ file_parser.cc
@@ -2,8 +2,11 @@
 #include <fstream>
 #include <cstdlib>
 #include <vector>
+#incluse <cctype>
 
 using namespace std;
+
+string file_name = argv[1]  
 
 struct line {
 
@@ -39,15 +42,46 @@ int size(){
   return victor.size();
 }
 
-/*  CS530 Fall 2017
-    How to read and write text files
-    Alan Riggins
-*/
-
-
 void print_error(string);
 
-int read_file() {
+/*
+ * Reads the file from the command line and parses through the input.
+ * @param file_name: assembly code source file.
+ * 
+ */
+void file_parser(file_name){
+	
+	read_file(); //loads contents with indexed lines.
+	
+	//loop to parse through contents line by line. Each line is passed through the line_parser.
+	for(int i =0; i<contents.size;i++){
+		line_parser(contents[i]);
+	}
+	return 0;
+}
+
+/*
+ * Parse through a raw line and assign the words to a line structure
+ * the line structure will then be pushed onto victor.
+ * @param raw_line: string containing the entire line to be parsed.
+ */
+void line_parser(string raw_line){
+	line tmp_line;	//temporary struct to be pushed onto victor.
+	string* raw_index;  //pointer for the raw_line
+	
+	//Skips over spaces
+	while(!isblank(*raw_index)){
+		//there is probably a fancier c++ way to do this 
+	}
+	
+	
+}
+
+/*
+ * Takes the file assigned to file_name 
+ * iterates line by line and assigns each to an index to vector contents
+ */
+void read_file() {
     ifstream infile; // input stream
     ofstream outfile; // output stream
     vector<string> contents;  // file contents
@@ -61,7 +95,6 @@ int read_file() {
     if(!infile)
         print_error("Sorry, could not open the file for reading");
 
-        //start parser here
     while(!infile.eof()) {
         getline(infile,line);
         contents.push_back(line);
@@ -69,19 +102,22 @@ int read_file() {
     infile.close();
 
 }
-
-void print_file(){  //in progress
+/**
+ * prints the indexes of vector victor
+ * Note: May need to be fixed to iterate through the line struct at each index (works for strings atm)
+ */
+void print_file(){ 
 
     cout << "Now dumping what we read from file ..." << endl;
-    for(int i=0; i < contents.size(); i++)
-        cout << contents[i] << endl;
+    for(int i=0; i < victor.size(); i++)
+        cout << victor[i] << endl;
 
     outfile.open("output.txt",ios::out);
     if(!outfile)
         print_error("Sorry, could not open the file for writing");
 
-    for(int i=0; i < contents.size(); i++)
-        outfile << contents[i] << endl;
+    for(int i=0; i < victor.size(); i++)
+        outfile << victor[i] << endl;
     outfile.close();
 
 }
