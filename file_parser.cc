0a488a93c3a4f7161076c1b3989f089edbb6605a
William Fox <wfox96@gmail.com>
2017-09-26 12:23:10 -0700

    Constructor Fix? Corrected Style for struct getters, outstream scope half fix? (#38)
    * made str args const, fixed style on getters for struct
    
    * it's string no tring, also constructor no longer gives error, only everything else
    
    * fixed typo, now trying to call structs getter, but I don't know the propersyntax, compiler still mad, but it knows what they are now, pls help

--- file_parser.cc
+++ file_parser.cc
@@ -4,14 +4,13 @@
 #include <vector>
 #include <cctype>
 #include <iomanip>
+#include <string>
 
 #include "file_parser.h"
 
 using namespace std;
 
-file_parser(string file_name) {
 
-}
 
 struct line {
     string label;
@@ -26,32 +25,32 @@ struct line {
         comment = "";
     }
 
-    string getLabel() const;
+    string getlabel() const;
 
-    string getOpcode() const;
+    string getopcode() const;
 
-    string getOperand() const;
+    string getoperand() const;
 
-    string getComment() const;
+    string getcomment() const;
 };
 
 vector<line> victor; //add new elements with .push_back(<line>);
 vector<string> contents;  // file contents in string form
 string file_name; // Class variable for file_name
 
-string line::getLabel() const {
+string line::getlabel() const {
     return label;
 }
 
-string line::getOpcode() const {
+string line::getopcode() const {
     return opcode;
 }
 
-string line::getOperand() const {
+string line::getoperand() const {
     return operand;
 }
 
-string line::getComment() const {
+string line::getcomment() const {
     return comment;
 }
 
@@ -66,14 +65,22 @@ void read_file();
 
 line line_parser(const string &raw_line);
 
+/**
+* Constructor
+*
+*
+*/
 
+file_parser::file_parser(const string file_name) {
+
+}
 
 /**
  * Reads the file from the command line and parses through the input.
  * @param file_name: assembly code source file.
  *
  */
-void file_parser(string parse_name) {
+void file_parser(const string parse_name) {
     file_name = std::move(parse_name);
     read_file(); //loads contents with indexed lines.
 
@@ -137,10 +144,10 @@ ostream &operator<<(ostream &out, const line &value) {
     const int opcode_col_width = 8;
     const int operand_col_width = 8;
 
-    out << setw(label_col_width) << ios::left << setfill(' ') << label;
-    out << setw(opcode_col_width) << ios::left << setfill(' ') << opcode;
-    out << setw(operand_col_width) << ios::left << setfill(' ') << operand_col_width;
-    out << comment << endl;
+    out << setw(label_col_width) << ios::left << setfill(' ') << &line.getlabel();
+    out << setw(opcode_col_width) << ios::left << setfill(' ') << &line.getopcode();
+    out << setw(operand_col_width) << ios::left << setfill(' ') << &line.getoperand();
+    out << &line.getcomment() << endl;
 
     return out;
 }
