aa6412c574c9322396754240219d211f0bb53a06
Alexander Pearson-Goulart <pearsongoulart@gmail.com>
2017-09-29 11:07:33 -0700

    Finished line parser and updated constructor and read_file functionalities to match the program specifications
    * Moved private class variables to header file; started working on file_parser and line_parser
    
    * Fixed ALL of the errors
    
    * Started on converting string line into line struct
    
    * Updated
    
    * Finished line parser and updated constructor and read_file functionalities to match the program specifications
    
    * Update file_parser.cc

--- file_parser.cc
+++ file_parser.cc
@@ -63,49 +63,181 @@ void print_error(const string &);
 
 void read_file();
 
-line line_parser(const string &raw_line);
+line line_parser(string);
 
 /**
-* Constructor
-*
-*
-*/
-
-file_parser::file_parser(const string file_name) {
-
-}
-
-/**
- * Reads the file from the command line and parses through the input.
+ * Constructor
+ * Reads the file from the command line and saves it locally
  * @param file_name: assembly code source file.
  *
  */
-void file_parser(const string parse_name) {
-    file_name = std::move(parse_name);
-    read_file(); //loads contents with indexed lines.
 
-    //loop to parse through contents line by line. Each line is passed through the line_parser.
-    for (int i = 0; i < contents.size(); i++) {
-        line_parser(contents[i]);
-    }
+file_parser::file_parser(const string file_name) {
+    file_name = std::move(file_name);
 }
+
+
+// void file_parser(string parse_name) {
+//     file_name = std::move(parse_name);
+//     read_file(); //loads contents with indexed lines.
+
+//     //loop to parse through contents line by line. Each line is passed through the line_parser.
+//     for (int i = 0; i < contents.size(); i++) {
+//         line_parser(contents[i]);
+//     }
+// }
 
 /**
  * Parse through a raw line and assign the words to a line structure
  * the line structure will then be pushed onto victor.
  * @param raw_line: string containing the entire line to be parsed.
  */
-line line_parser(const string &raw_line) {
+line line_parser(string raw_line) {
     line tmp_line;    //temporary struct to be pushed onto victor.
-    string *raw_index;  //pointer for the raw_line
 
-    //Skips over spaces
-    // while (!isblank(*raw_index)) {
-    //     //there is probably a fancier c++ way to do this
-    // }
+    string token;
 
+    string delimiters = " \t\n";
 
-    // TODO Implement conversion from string to line
+    if(!raw_line.empty()) {
+        int column_start = 0;
+        int col_space; // Distance to start of next column
+        int tok_first;
+        int tok_last;
+
+        int column_num = 1; // Which column are you in
+
+        bool isQuote = false;
+
+        string column;     
+
+        while(column_num < 5) {
+
+            if(column_start > raw_line.length()) {
+                if(column_num == 4)
+                    tmp_line.comment = "";
+                break;
+            }
+
+            if(raw_line.find_first_of(" \t") > raw_line.length()) {
+                tmp_line.label = raw_line;
+                tmp_line.opcode = "";
+                tmp_line.operand = "";
+                tmp_line.comment = "";
+                break;
+            }
+
+            column = raw_line.substr(column_start, 8);
+
+            tok_last = column.find_first_not_of(delimiters, 0); // Start of token
+            tok_first = column.find_first_of(delimiters, tok_last); // End of token
+
+            if(column_num == 1 && (tok_last != -1)) {
+
+                char first_letter = column[0];
+        
+                // Check if the column is a comment
+                if(first_letter == '.') {
+                    tmp_line.comment = raw_line.substr(column_start, raw_line.length() - column_start);
+                    break;
+                }
+
+                tmp_line.label = column.substr(tok_last, tok_first-tok_last);
+            }
+            else if(column_num == 1 && tok_last == -1) {
+                tmp_line.label = "";
+            }
+            
+            if(column_num == 2 && (tok_last != -1)) {
+
+                char first_letter = column[0];
+                
+                // Check if the column is a comment
+                if(first_letter == '.') {
+                    tmp_line.comment = raw_line.substr(column_start, raw_line.length() - column_start);
+                    break;
+                }
+
+                tmp_line.opcode = column.substr(tok_last, tok_first-tok_last);
+            }
+            else if(column_num == 2 && tok_last == -1) {
+                tmp_line.opcode = "";
+            }
+            
+            if(column_num == 3 && (tok_last != -1)) {
+
+                char first_letter = column[0];
+                
+                // Check if the column is a comment
+                if(first_letter == '.') {
+                    tmp_line.comment = raw_line.substr(column_start, raw_line.length() - column_start);
+                    break;
+                }
+
+                if(column[1] == '\'') {
+                    isQuote = true;
+                }
+
+                tmp_line.operand = column.substr(tok_last, tok_first-tok_last);
+            }
+            else if(column_num == 3 && tok_last == -1) {
+                tmp_line.operand = "";  
+            }
+            
+            if(column_num == 4 && (tok_last != -1)) {
+
+                char first_letter = column[0];
+                
+                // Check if the column is a comment, throw error if not
+                if(first_letter == '.') {
+                    tmp_line.comment = raw_line.substr(column_start, raw_line.length() - column_start);
+                    break;
+                }
+                else {
+                    cout << "ERROR: Expected a comment." << endl;
+                }
+            }
+            else if(column_num == 4 && tok_last == -1) {
+                tmp_line.comment = "";  
+            }
+
+            // If the column is blank, move on to the next one.
+            if(tok_last == -1) {
+                column_start = column_start + 8;
+                column_num = column_num + 1;
+            }
+            else {
+
+                // If the column takes up all 8 characters, find the next delimiter/white space
+                if(tok_first == -1) {
+                    int column_end = raw_line.find_first_of(delimiters, (column_start + 8));
+                    col_space = raw_line.find_first_not_of(delimiters, column_end);
+                }
+                else if(isQuote) {
+                    // index of the last quote
+                    int end_quote = raw_line.find_first_of('\'', (column_start + tok_first));
+
+                    // index of where the next column begins after end of quote marks
+                    int column_end = raw_line.find_first_of(delimiters, end_quote);
+                    col_space = raw_line.find_first_not_of(delimiters, column_end);
+                    isQuote = false;
+                }
+                else {
+                    col_space = raw_line.find_first_not_of(delimiters, (column_start + tok_first));
+                }
+                
+                column_start = col_space;
+                column_num = column_num + 1;
+            }
+        }
+    }
+    else {
+        tmp_line.label = "";
+        tmp_line.comment = "";
+        tmp_line.opcode = "";
+        tmp_line.operand = "";
+    }
+
     return tmp_line;
 }
 
@@ -130,6 +262,12 @@ void read_file() {
         contents.push_back(line);
     }
     infile.close();
+
+    //loop to parse through contents line by line. Each line is passed through the line_parser.
+    for (int i = 0; i < contents.size(); i++) {
+        victor.push_back(line_parser(contents[i]));
+    }
+
 }
 
 /**
