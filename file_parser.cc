dd7abd9a7deaf9472e90ae36aff7f17244818607
Neha Nene <nehanene15@gmail.com>
2017-09-30 11:10:07 -0700

    Update file_parser.cc (#62)

--- file_parser.cc
+++ file_parser.cc
@@ -299,7 +299,7 @@ void file_parser::print_file() {
     }
 }
 
-string fileparser::get_token(unsigned int row, unsigned int column) {
+string file_parser::get_token(unsigned int row, unsigned int column) {
     if (column == 0) {                  //   0      1       2        3
         return victor.at(row).label;    //(label/opcode/operands/comments)
     } else if (column == 1) {
