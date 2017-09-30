a23756d94c0f4b42439fe325e8c80ac0d0e41249
Alexander Pearson-Goulart <pearsongoulart@gmail.com>
2017-09-30 09:36:25 -0700

    Got rid of print_error()

--- file_parser.cc
+++ file_parser.cc
@@ -291,11 +291,6 @@ void print_file() {
     }
 }
 
-void print_error(const string &s) {
-    cout << s << endl;
-    exit(1);
-}
-
 string get_token(unsigned int row, unsigned int column) {
     if (column == 0) {                  //   0      1       2        3
         return victor.at(row).label;    //(label/opcode/operands/comments)
