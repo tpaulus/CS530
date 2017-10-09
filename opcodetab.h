ae7134d7cb1c8bc5628b28de60e31aa86de3a13b
William Fox <wfox96@gmail.com>
2017-10-08 18:31:14 -0700

    Created opcodetab.h, should match the .cc

---
+++ opcodetab.h
@@ -0,0 +1,48 @@
+/*
+ * Alexander Pearson-Goulart, Athena Campbell, Jon Verne, Neha Nene, Tom Paulas, William Fox
+ * cssc1015
+ * prog1
+ * CS530, Fall 2017
+ *
+*/
+
+#ifndef OPCODETAB_H
+#define OPCODETAB_H
+
+#include<string>
+#include<map>
+//#include<pair> //Not Sure about this yet
+
+using namespace std;
+
+class opcodetab {
+    public:
+        // ctor
+        // creates a new dictionary structure and loads all of the opcodes for 
+        // the SIC/XE architecture into the table.  Use the STL
+        // map for this.
+        opcodetab(); 
+        
+        // takes a SIC/XE opcode and returns the machine code 
+        // equivalent as a two byte string in hexadecimal.
+        // Example:  get_machine_code("ADD") returns the value 18
+        // Note that opcodes may be prepended with a '+'.
+        // throws an opcode_error_exception if the opcode is not 
+        // found in the table.
+        string get_machine_code(string);  
+        
+        // takes a SIC/XE opcode and returns the number of bytes 
+        // needed to encode the instruction, which is an int in
+        // the range 1..4.
+        // NOTE: the opcode must be prepended with a '+' for format 4.
+        // throws an opcode_error_exception if the opcode is not 
+        // found in the table.        
+        int get_instruction_size(string);
+                        
+    private:
+        // your variables and private methods go here
+        map<string,pair<string, int>> marvin;
+
+};
+
+#endif
