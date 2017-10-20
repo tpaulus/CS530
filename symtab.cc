d7975a547488b5be8677603d6a872b404d2d7d9c
William Fox <wfox96@gmail.com>
2017-10-19 19:04:29 -0700

    Added basic symtab files
    Doens't throw exceptions

---
+++ symtab.cc
@@ -0,0 +1,70 @@
+/*
+ * Alexander Pearson-Goulart, Athena Campbell, Jon Verne, Neha Nene, Tom Paulus, William Fox
+ * cssc1015
+ * Team Virginia
+ * prog3
+ * CS530, Fall 2017
+ *
+*/
+
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include "opcodetab.h"
+
+using namespace std;
+
+map <string, pair<string, bool> > symbol_table;
+
+string to_upper(string s) {
+    transform(s.begin(), s.end(), s.begin(), ::to_upper);
+    return s;
+}
+
+symtab::symtab() {
+    //TODO: Does anything belong in here?
+}
+
+void symtab::insert(pair<string, pair<string, bool> > obj) {
+    obj.first = to_upper(obj.first);
+    symbol_table.insert(obj);
+
+    //TODO: Should only insert if it doesn't exist?
+}
+
+void symtab::insert(string string1, string string2, bool is_r) {
+    symtab::insert(pair<string1, pair< string2, bool> >);
+}
+
+void symtab::update(pair<string, pair<string, bool> > obj) {
+    map<string, pair<string, bool> >::iterator m_iter = symbol_table.find(toupper(obj.first));
+    //if key was found
+    if (m_iter != m.end()){
+        m_iter->second = obj.second;
+    }
+    //TODO: Else throw exception?
+}
+
+void symtab::update(string string1, string string2, bool is_r) {
+    symtab::update(pair<string1, pair <string2, is_r> >);
+}
+
+bool symtab::contains(string key) {
+    return symbol_table.find(to_upper(key)) != symbol_table.end();
+}
+
+string symtab::get_value(string string1) {
+    if(symtab::contains(string1)){
+        return symbol_table.at(string1).first;
+    }
+    //TODO: Else throw exception?
+}
+
+bool symtab::is_relative(string string1) {
+    if(symtab::contains(string1)){
+        return symbol_table.at(string1).first;
+    }
+    //TODO: Else throw exception?
+}
