3127e42bda9417813ba3ce14518dadd2f0052503
William Fox <wfox96@gmail.com>
2017-10-19 20:26:54 -0700

    Added Sicxe_asm

---
+++ sicxe_asm.cc
@@ -0,0 +1,20 @@
+/*
+ * Alexander Pearson-Goulart, Athena Campbell, Jon Verne, Neha Nene, Tom Paulus, William Fox
+ * cssc1015
+ * Team Virginia
+ * prog3
+ * CS530, Fall 2017
+ *
+*/
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+#include "sicxe_asm.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string filename = argv[1];
+
+}
---
+++ sicxe_asm.h
@@ -0,0 +1,29 @@
+/*
+ * Alexander Pearson-Goulart, Athena Campbell, Jon Verne, Neha Nene, Tom Paulus, William Fox
+ * cssc1015
+ * Team Virginia
+ * prog3
+ * CS530, Fall 2017
+ *
+*/
+
+#ifndef SICXE_ASM_H
+#define SICXE_ASM_H
+
+#include <string>
+
+using namespace std;
+
+class sicxe_asm {
+
+public:
+
+private:
+
+    string filename;
+
+};
+
+
+
+#endif //SICXE_ASM_H
