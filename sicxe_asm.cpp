302c53686c1f903aff34ae4568c07f055db2e147
William Fox <wfox96@gmail.com>
2017-10-29 18:09:32 -0700

    Gets to Start in Flow Chart (#121)
    * Removed old struct
    
    * added inline helpers
    
    * added string to int
    
    * Got to start

--- sicxe_asm.cpp
+++ sicxe_asm.cpp
@@ -9,86 +9,57 @@
 #include <string>
 #include <iostream>
 #include <iomanip>
+#include <stdlib.h>
 
 #include "sicxe_asm.h"
-
 using namespace std;
 
-struct listing_line {
-    string address;
-    unsigned int linenum;
-    string label;
-    string opcode;
-    string operand;
-    string machinecode;
-
-    string getaddress() const;
-    unsigned int getlinenum() const;
-    string getlabel() const;
-    string getopcode() const;
-    string getoperand() const;
-    string getmachinecode() const;
-
-    listing_line(void){
-        address="";
-        linenum=0; //TODO check if 0 is the correct initializer in C++
-        label="";
-        opcode="";
-        operand="";
-        machinecode="";
-    }
-    ~listing_line(){
-    }
-
-};
-
-string sicxe_asm::listing_line::getaddress() const {
-    return address;
-}
-
-unsigned int sicxe_asm::listing_line::getlinenum() const {
-    return linenum;
-}
-
-string sicxe_asm::listing_line::getlabel() const {
-    return label;
-}
-
-string sicxe_asm::listing_line::getopcode() const {
-    return opcode;
-}
-
-string sicxe_asm::listing_line::getoperand() const {
-    return operand;
-}
-
-string sicxe_asm::listing_line::getmachinecode() const {
-    return machinecode;
-}
-
-bool is_comment_or_empty(listing_line line);
+bool is_comment_or_empty(file_parser::formatted_line line);
 
 int main(int argc, char *argv[]) {
+    if(argc != 2) {
+        cout << "Error, you must supply the name of the file to assemble." << endl;
+        exit(1);
+    }
+    string filename = argv[1];
 
-    string filename = argv[1];  //TODO: Need to check if this exists
     file_parser parser(filename);
-    parser.read_file();
-    vector <listing_line> listing_vector(100);
-    listing_line new_line;
-
-    for(unsigned int i = 0; i < parser.size(); i++) {
-        //Loads new line from file_parser
-        new_line.linenum = i + 1; //Source files are one based
-        new_line.label = parser.get_token(i,0);
-        new_line.opcode = parser.get_token(i,1);
-        new_line.operand = parser.get_token(i,2);
-        if(is_comment_or_empty(new_line))
-            continue;
-        listing_vector.push_back(new_line);
+    try{
+        parser.read_file();
+    }catch (file_parse_exception fileParseException){
+        cout << "ERROR - " << fileParseException.getMessage() << endl;
+        exit(1);
+    }
+    vector <file_parser::formatted_line> listing_vector(0);
+    vector<file_parser::formatted_line>::iterator line_iter;
+    //Loads new Vector of Structs
+    for(int i = 0; i < parser.size(); i++) {
+        listing_vector.push_back(parser.get_struct((unsigned int)i));
     }
 
+    line_iter = listing_vector.begin(); //Grabs first line
+    //While more lines and operand != start
+    while(line_iter != listing_vector.end() && sicxe_asm::to_uppercase(line_iter->opcode) != "START"){
+        if(!is_comment_or_empty(*line_iter)){
+            cout << "ERROR - No executable code before START Directive: Line " << line_iter->linenum << endl;
+            cout << *line_iter << endl;
+            exit(2);
+        }
+        line_iter++; //Grab next line
+    }
+    if(line_iter == listing_vector.end()){
+        cout << "ERROR - No START Directive" << endl; //We can't know the line number, following the iterator is nullptr
+        exit(3);
+    }
+    //Get Program name and starting location counter
+    string program_name = line_iter->label;
+    int location_counter = sicxe_asm::hex_to_int(line_iter->operand);
+    //TODO: This is now where start is in the flowchart
+    
+
 }
 
-bool is_comment_or_empty(listing_line line) {
+bool is_comment_or_empty(file_parser::formatted_line line) {
     return line.label.empty() && line.opcode.empty() && line.operand.empty();
 }
+
--- sicxe_asm.h
+++ sicxe_asm.h
@@ -12,9 +12,17 @@
 
 #include <string>
 #include <vector>
+#include <cstdio>
+#include <sstream>
+#include <algorithm>
 
 #include "file_parser.h"
+#include "file_parse_exception.h"
 #include "opcodetab.h"
+#include "opcode_error_exception.h"
+#include "symtab.h"
+#include "symtab_exception.h"
+
 
 using namespace std;
 
@@ -22,37 +30,45 @@ class sicxe_asm {
 
 public:
 
+    inline static int string_to_int(string s){
+        istringstream instr(s);
+        int n;
+        instr >> n;
+        return n;
+    }
+
+    inline static int hex_to_int(string s){
+        int value;
+        sscanf(s.c_str(), "%x", &value);
+        return value;
+    }
+
+    inline static int dec_to_int(string s){
+        int value;
+        sscanf(s.c_str(), "%d", &value);
+        return value;
+    }
+
+    inline static string to_uppercase(string s){
+        transform(s.begin(), s.end(), s.begin(), ::toupper);
+        return s;
+    }
+    inline static string int_to_hex(int num, int width){
+        stringstream out;
+        out << setw(width) << setfill('0') << hex << num;
+        return to_uppercase(out.str());
+    }
+
 private:
 
-    struct listing_line{
-        string address;
-        unsigned int linenum;
-        string label;
-        string opcode;
-        string operand;
-        string machinecode;
-        
-        inline listing_line(){
-        address="";
-        linenum=0; 
-        label="";
-        opcode="";
-        operand="";
-        machinecode="";
-        }
-            
-        std::string getaddress() const;
-        unsigned int getlinenum() const;
-        std::string getlabel() const;
-        std::string getopcode() const;
-        std::string getoperand() const;
-        std::string getmachinecode() const;
-        
-    };
     string filename;
     file_parser parser;
-    vector<listing_line> list_vec;
-    listing_line tmp_line;
+    vector<file_parser::formatted_line> list_vec;
+    vector<file_parser::formatted_line>::iterator line_iter;
+    string program_name;
+    int location_counter;
+
+
 
 };
 
